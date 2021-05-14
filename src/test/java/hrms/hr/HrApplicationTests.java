f1372a0d61de1ea22778d610e7236b5a0c9832d2
Aytac Agma <aytac.agma@netone.net.tr>
2021-05-14 21:19:47 +0300

    Spring Boot initial commit

---
+++ src/test/java/hrms/hr/HrApplicationTests.java
@@ -0,0 +1,13 @@
+package hrms.hr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
